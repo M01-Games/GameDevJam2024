ca263aa334e970089f89836c7b4894c9e7d35996
syafiqoarashy <101589777+syafiqoarashy@users.noreply.github.com>
2024-05-27 01:48:58 +1000

    Implement first person controller prototype

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float walkSpeed = 6f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+
+    public float sensitivity = 2f;
+    public float lookXLimit = 80f;
+
+    float rotationX = 0f;
+    public bool canMove = true;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();  
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = canMove ? walkSpeed * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? walkSpeed * Input.GetAxis("Horizontal") : 0;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * sensitivity;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        }
+    }
+}
