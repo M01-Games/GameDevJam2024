5114deca34858e8691f436c7a2794abd45e0781d
Raees Ellam <ellamraees6@gmail.com>
2024-06-03 14:28:27 +0100

    Audio Slider setting

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume); 
     }
 }
